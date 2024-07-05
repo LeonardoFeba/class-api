15a751e96115a848e4bf728e99aeb99c071cefc8
Leonardo Feba <febaleonardo@gmail.com>
2024-07-05 19:44:02 -0300

    feat add pokemon get method

---
+++ src/api/pokedex/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function GetPokemon() {
+  const [pokemon, setPokemon] = useState("");
+  const [poki, setPoki] = useState(null);
+
+  async function handleSearch() {
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+      );
+      setPoki(response.data);
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Digite o nome de um pokemon:</h1>
+      <input
+        type="text"
+        onChange={(e) => {
+          setPokemon(e.target.value);
+        }}
+      ></input>
+      <button onClick={handleSearch}>Eu escolho você!</button>
+      {poki && (
+        <div>
+          <img src={poki.sprites.front_default} />
+        </div>
+      )}
+    </div>
+  );
+}
